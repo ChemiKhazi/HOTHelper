9499e96efb491c92dad806f31d2482c002ca5b1d
Jeiel Aranal <jeiel.aranal@gmail.com>
2014-08-11 00:12:52 +0800

    Initial commit.

---
+++ HOTHelper.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Holoville.HOTween;
+using Holoville.HOTween.Core;
+
+namespace kontrabida.hothelper
+{
+	public class HOTWeenHelper
+	{
+		/// <summary>
+		/// Target object of this tween
+		/// </summary>
+		public object Target { get; protected set; }
+		/// <summary>
+		/// The actual wrapped HOTween object
+		/// </summary>
+		public Tweener Tween { get; protected set; }
+		/// <summary>
+		/// The tweening parameters built by this class
+		/// </summary>
+		public TweenParms Params { get; protected set; }
+		/// <summary>
+		/// The time value that will be passed to HOTween
+		/// </summary>
+		public float? Time { get; protected set; } 
+
+		/// <summary>
+		/// Indicates if this HOTween has all the data needed to play
+		/// </summary>
+		public bool CanRun
+		{
+			get
+			{
+				if (Params == null)
+					return false;
+				if (!Time.HasValue)
+					return false;
+				return true;
+			}
+		}
+
+		/// <summary>
+		/// Accessor for creating the Params and then using it immediately
+		/// </summary>
+		protected TweenParms ParamUtil
+		{
+			get
+			{
+				if (Params == null)
+					Params = new TweenParms();
+				return Params;
+			}
+		}
+
+		public HOTWeenHelper(object target)
+		{
+			Target = target;
+		}
+
+		/// <summary>
+		/// The time this tween will take.
+		/// </summary>
+		/// <param name="duration"></param>
+		/// <returns></returns>
+		public HOTWeenHelper Duration(float duration)
+		{
+			Time = duration;
+			return this;
+		}
+
+		/// <summary>
+		/// Sets the time of the tween and sets SpeedBased to true
+		/// </summary>
+		/// <param name="speed"></param>
+		/// <returns></returns>
+		public HOTWeenHelper Speed(float speed)
+		{
+			Time = speed;
+			ParamUtil.SpeedBased(true);
+			return this;
+		}
+
+		/// <summary>
+		/// The easing function for this tween
+		/// </summary>
+		/// <param name="easing"></param>
+		/// <returns></returns>
+		public HOTWeenHelper Ease(EaseType easing)
+		{
+			ParamUtil.Ease(easing);
+			return this;
+		}
+
+		/// <summary>
+		/// Key/pair values for properties to tween on the target object, can take more than one key/pair
+		/// </summary>
+		/// <param name="properties">Must be in the format: "propName1", valueOfProp1, "propName2", valueOfProp2...</param>
+		/// <returns></returns>
+		public HOTWeenHelper Props(params object[] properties)
+		{
+			if ((properties.Length%2) != 0)
+				throw new Exception("Property Key/Value pair count does not match");
+			for (int i = 0; i < properties.Length; i += 2)
+			{
+				string propName = (string)properties[i];
+				var propVal = properties[i + 1];
+				ParamUtil.Prop(propName, propVal);
+			}
+			return this;
+		}
+
+		/// <summary>
+		/// Set a callback for this tween
+		/// </summary>
+		/// <param name="callback"></param>
+		/// <returns></returns>
+		public HOTWeenHelper OnComplete(TweenDelegate.TweenCallback callback)
+		{
+			ParamUtil.OnComplete(callback);
+			return this;
+		}
+
+		/// <summary>
+		/// Set a callback with parameters for this tween
+		/// </summary>
+		/// <param name="callback"></param>
+		/// <returns></returns>
+		public HOTWeenHelper OnComplete(TweenDelegate.TweenCallbackWParms callback)
+		{
+			ParamUtil.OnComplete(callback);
+			return this;
+		}
+
+		/// <summary>
+		/// Creates the tween, which starts it. Call this at the end of the sequence.
+		/// </summary>
+		/// <returns></returns>
+		public Tweener Start()
+		{
+			if (Params == null)
+				return null;
+			if (!Time.HasValue)
+				return null;
+			Tween = HOTween.To(Target, Time.Value, Params);
+			return Tween;
+		}
+	}
+
+	public static class HOTHelperExtension
+	{
+		/// <summary>
+		/// Create a tween on this object.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		public static HOTWeenHelper Tween<T>(this T target)
+		{
+			return new HOTWeenHelper(target);
+		}
+	}
+
+}
---
+++ HOTSequence.cs
@@ -0,0 +1,80 @@
+﻿using Holoville.HOTween;
+using Holoville.HOTween.Core;
+using System.Collections.Generic;
+
+namespace kontrabida.hothelper
+{
+	public class HOTSequence
+	{
+		protected class TimeTweeen
+		{
+			public float time;
+			public IHOTweenComponent tween;
+
+			public TimeTweeen(float time, IHOTweenComponent tween)
+			{
+				this.time = time;
+				this.tween = tween;
+			}
+		}
+
+		public Sequence Sequence { get; protected set; }
+		public SequenceParms Params { get; protected set; }
+
+		public bool IsReady { get { return Sequence != null; }}
+
+		protected SequenceParms ParamUtil
+		{
+			get
+			{
+				if (Params == null)
+					Params = new SequenceParms();
+				return Params;
+			}
+		}
+
+		protected List<TimeTweeen> insertTweens = new List<TimeTweeen>();
+		protected List<IHOTweenComponent> appendTweens = new List<IHOTweenComponent>(); 
+
+		public HOTSequence OnComplete(TweenDelegate.TweenCallback callback)
+		{
+			ParamUtil.OnComplete(callback);
+			return this;
+		}
+
+		public HOTSequence OnComplete(TweenDelegate.TweenCallbackWParms callback)
+		{
+			ParamUtil.OnComplete(callback);
+			return this;
+		}
+
+		public HOTSequence Insert(float time, HOTWeenHelper tween)
+		{
+			insertTweens.Add(new TimeTweeen(time, tween.Start()));
+			return this;
+		}
+
+		public HOTSequence Append(HOTWeenHelper tween)
+		{
+			appendTweens.Add(tween.Start());
+			return this;
+		}
+
+		public Sequence BuildSequence()
+		{
+			if (!IsReady)
+			{
+				Sequence = new Sequence(Params);
+				appendTweens.ForEach(tween => Sequence.Append(tween));
+				insertTweens.ForEach(tween => Sequence.Insert(tween.time, tween.tween));
+			}
+			return Sequence;
+		}
+
+		public Sequence Play()
+		{
+			BuildSequence().Play();
+			return Sequence;
+		}
+	}
+}
